3570057df7ab3304a071111813d5a01408ffd78a
Vignesh Siva <vignesh.siva@finstein.ai>
2026-09-23 17:05:10 +0530

    New Changes

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-print("Hello World")
 
 rows = int(input("Enter number of rows: "))
-
 for i in range(rows):
     for j in range(i+1):
         print(j+1, end=" ")
     print()
 
-text = input("Enter a string: ")
-
-if text == text[::-1]:
+txt = input("Enter a string: ")
+if txt == txt[::-1]:
     print("Palindrome")
 else:
     print("Not a Palindrome")
 
-word = input("Enter a word: ")
+while True:
+    print("Hello world")
 
+word = input("Enter a word: ")
 if word == word[::-1]:
     print("Palindrome")
 else:
     print("Not a palindrome")
+
